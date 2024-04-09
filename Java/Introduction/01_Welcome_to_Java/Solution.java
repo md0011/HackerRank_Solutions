8fe445e957bb0a810c001dae88183802e438480c
md0011 <mayurd0303@gmail.com>
2024-04-09 14:40:18 +0530

    Hello World!

---
+++ Java/Introduction/01_Welcome_to_Java/Solution.java
@@ -0,0 +1,20 @@
+/*
+Input Format
+
+There is no input for this challenge.
+
+Output Format
+
+You must print two lines of output:
+Print Hello, World. on the first line.
+Print Hello, Java. on the second line
+
+SOLUTION->
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
